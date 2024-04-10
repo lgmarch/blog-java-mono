40f41da3aced4918e7d259697082c6b9cfc0ec75
lmarch <lmarch@yandex.ru>
2024-04-10 19:26:18 +0300

    Init Project.

---
+++ src/main/java/com/march/blog/BlogApplication.java
@@ -0,0 +1,18 @@
+package com.march.blog;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(BlogApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+		LOGGER.info("* Blog Started *");
+	}
+
+}
